ef20ecb4824c0cb31d49a93008c9d0057005513c
daniizzulhaq <daniizzulhaq2002tkj@gmail.com>
2026-07-17 12:04:25 +0700

    edit nama

--- resources/views/member/layouts/app.blade.php
+++ resources/views/member/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ $appConfig['app_logo']['value'] }}" />
-    <title>Hi Tech Exchange</title>
+    <title>idngloex</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
